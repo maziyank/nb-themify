c4d1dd3b42a41adc0215deeeb51578e5762176a8
maziyank <maziyank@gmail.com>
2021-08-02 11:51:00 +0700

    update docs

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ define(["require",
 ], function (requirejs, Jupyter) {
     'use strict';
 
+    // register theme list here
     const theme_list = [{
         name: 'MOF-DAC 1',
         css: requirejs.toUrl("./css/mofdac.css"),
@@ -20,14 +21,17 @@ define(["require",
         fonts: ["https://fonts.googleapis.com/css2?family=Ubuntu&display=swap"]
     }];    
 
+    // read notebook metadata, if not present set to default
     const read_theme_from_config = function () {
         return IPython.notebook.metadata.themify || { theme: 'default' };
     };    
 
+    // create menu item element
     const menu_item = (text, clickHandler) => {
         return $(`<li><a href="#" class="menu-shortcut-container"><span class="action mofdaclink">${text}</span></a><ul class="dropdown-menu"><li>`).click(clickHandler)
     }
 
+    // ini themify menu to toolbar 
     const init_toolbar = () => {
         const menu_bar = $(Jupyter.menubar.element).find('ul.navbar-nav');
         const dropdown_menu = $('<ul class="dropdown-menu" role="menu" id="mofdac-theme-menu"/>');
@@ -43,12 +47,14 @@ define(["require",
         menu_bar.append(dropdown);
     }
 
+    // reset to default styles
     const reset_css= (event) => {
         $("#mofdac-theme").remove();
         $('.mofdaclink').css('font-weight','normal');
         if (event) $(event.target).css('font-weight','bold');
     }
 
+    // inject selected css
     const init_css = (event, theme) => {        
         reset_css(event);                
         $('head').append(`<link id="mofdac-theme" rel="stylesheet" href="${theme.css}" type="text/css" />`);         
@@ -59,6 +65,7 @@ define(["require",
         IPython.notebook.metadata['themify'] = { theme: theme.name }
     }     
 
+    // load extension
     const load_ipython_extension = () => {        
         init_toolbar();        
 
